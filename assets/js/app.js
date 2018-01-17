bc5ecb8d9d8c1f32ff8fcd8ad55e4960d698e5cf
Aza <azamatkurmanbekov@gmail.com>
2018-01-18 00:32:02 +0600

    add bootstrap material kit etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import  $ from 'jquery';
 import '../css/app.scss';
 
 $(document).ready(function () {
    ReactDom.render(
-       <h1 className="react3">From React3</h1>,
+       <h1 className="react3">From React6</h1>,
        $('#root')[0]
    );
 });
